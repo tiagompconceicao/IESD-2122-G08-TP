c5580e5c0c0a05e5b7d0991205ee27cb7cdb0b06
Tiago Conceição <41746440+tiagompconceicao@users.noreply.github.com>
2022-04-27 02:15:50 +0100

    Fixing Bugs

--- ISyIESD/CesTC/SerTM/SerTMOPE/src/main/java/isos/tp1/isyiesd/cestc/sertm/TransactionManager.java
+++ ISyIESD/CesTC/SerTM/SerTMOPE/src/main/java/isos/tp1/isyiesd/cestc/sertm/TransactionManager.java
@@ -2,25 +2,22 @@ package isos.tp1.isyiesd.cestc.sertm;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "isos.tp1.isyiesd.cesvector.servector.ITransactionManager")
+@WebService(endpointInterface = "isos.tp1.isyiesd.cestc.sertm.ITransactionManager")
 public class TransactionManager implements ITransactionManager {
 
 
     @Override
     public int tx_begin() {
-        //TODO Implement method
         return 0;
     }
 
     @Override
     public boolean tx_commit(int tid) {
-        //TODO Implement method
         return false;
     }
 
     @Override
     public boolean tx_rollback(int tid) {
-        //TODO Implement method
         return false;
     }
 }
