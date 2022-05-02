93295340282a81b0be67a963596c14606f077b15
Tiago Conceição <41746440+tiagompconceicao@users.noreply.github.com>
2022-05-02 17:42:49 +0100

    Migrating communication stack to gRPC, contracts done

--- ISyIESD/CesTC/SerTM/SerTMAPIM/src/main/java/isos/tp1/isyiesd/cesvector/servector/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://iesd21.isos.isyiesd.cesvector.servectorserver")
-package isos.tp1.isyiesd.cesvector.servector;
--- ISyIESD/CesVector/SerVector/SerVectorAPIM/src/main/java/isos/tp1/isyiesd/cestc/sertm/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://iesd21.isos.isyiesd.cestc.sertmserver")
-package isos.tp1.isyiesd.cestc.sertm;
